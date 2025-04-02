3605503fe8d8d135f9b976ee95333a6fc38db285
Lucas Marinho <marinho.dev98@gmail.com>
2025-04-01 21:57:22 -0300

    Ajustando o header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Alterna o menu hamburger
-    const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('nav-menu');
-  
-    hamburger.addEventListener('click', () => {
-      navMenu.classList.toggle('open');
-      hamburger.classList.toggle('active');
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  const currentPath = window.location.pathname;
+  const navLinks = document.querySelectorAll('.nav-menu ul li a');
+
+  navLinks.forEach(link => {
+    // Obtem o href do link
+    const linkPath = link.getAttribute('href');
+    // Se o linkPath for 'index.html' e o caminho atual for "/" ou "/index.html", marca-o como ativo
+    if ((currentPath === '/' || currentPath.endsWith('/index.html')) && linkPath === 'index.html') {
+      link.classList.add('active');
+    }
+    // Caso contrário, se o currentPath incluir o linkPath (ex.: "calculadora-renal.html")
+    else if (currentPath.includes(linkPath) && linkPath !== 'index.html') {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
   });
-  
+});
